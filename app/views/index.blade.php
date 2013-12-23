1d6f51cbf09754d92e31609eb31829560cc072ba
Cindy Leschaud <cindy.leschaud@gmail.com>
2013-12-23 10:16:55 +0100

    start design

---
+++ app/views/index.blade.php
@@ -0,0 +1,432 @@
+@extends('layouts.master')
+
+@section('content')
+	
+	
+			<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+			<!-- Main Content -->
+			<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+			<div class="main-content">
+				<div class="main-content-inner content-width">
+
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<!-- Left Aligned Icons & Text -->
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<div class="column-container">
+
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<!-- One Third -->
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<div class="column-one-third">
+							<div class="icons-column">
+								<!-- Icon Backing -->
+								<div class="icon-backing" style="background-color: #66b9b4;">
+									<!-- Icon -->
+									<i class="fa fa-heart"></i>
+								</div>
+							</div>
+							<div class="content-column">
+								<!-- Title -->
+								<h3>Timeless Design</h3>
+								<!-- Text -->
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting.</p>
+							</div>
+						</div>
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<!-- END One Third -->
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<!-- One Third -->
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<div class="column-one-third">
+							<div class="icons-column">
+								<!-- Icon Backing -->
+								<div class="icon-backing" style="background-color: #6E6588;">
+									<!-- Icon -->
+									<i class="fa fa-font"></i>
+								</div>
+							</div>
+							<div class="content-column">
+								<!-- Title -->
+								<h3>Great Typography</h3>
+								<!-- Text -->
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting.</p>
+							</div>
+						</div>
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<!-- END One Third -->
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<!-- One Third -->
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<div class="column-one-third">
+							<div class="icons-column">
+								<!-- Icon Backing -->
+								<div class="icon-backing" style="background-color: #63885F;">
+									<!-- Icon -->
+									<i class="fa fa-fullscreen"></i>
+								</div>
+							</div>
+							<div class="content-column">
+								<!-- Title -->
+								<h3>Responsive Layout</h3>
+								<!-- Text -->
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting.</p>
+							</div>
+						</div>
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<!-- END One Third -->
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+					</div>
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<!-- END Left Aligned Icons & Text -->
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<!-- Divider -->
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<div class="divider"></div>
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<!-- END Divider -->
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<!-- Latest Work -->
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<!-- Title -->
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<h3 class="section-title">Latest Work</h3>
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<!-- END Title -->
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<!-- Navigation (Back/Next) -->
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<div id="portfolio-nav" class="carousel-nav">
+						<div class="back"></div>
+						<div class="next"></div>
+					</div>
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<!-- END Navigation (Back/Next) -->
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<!-- The Carousel -->
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<div class="carousel">
+						<ul id="portfolio-carousel" class="slider-container">
+
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- One Fourth -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<li class="column-one-fourth portfolio-item branding">
+								<!-- Image -->
+								<a href="portfolio-detail.html" class="image-link"><img alt="" src="images/placeholders/placeholder-large.jpg" class="fullwidth"/></a>
+								<!-- Title -->
+								<h3><a href="portfolio-detail.html">ATEX Sportsware</a></h3>
+								<!-- Tags -->
+								<div class="tags">Branding</div>
+							</li>
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- END One Fourth -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- One Fourth -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<li class="column-one-fourth portfolio-item coding">
+								<!-- Image -->
+								<a href="portfolio-detail.html" class="image-link"><img alt="" src="images/placeholders/placeholder-large.jpg" class="fullwidth"/></a>
+								<!-- Title -->
+								<h3><a href="portfolio-detail.html">Farmers Insurance</a></h3>
+								<!-- Tags -->
+								<div class="tags">Code Development</div>
+							</li>
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- END One Fourth -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- One Fourth -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<li class="column-one-fourth portfolio-item branding identity">
+								<!-- Image -->
+								<a href="portfolio-detail.html" class="image-link"><img alt="" src="images/placeholders/placeholder-large.jpg" class="fullwidth"/></a>
+								<!-- Title -->
+								<h3><a href="portfolio-detail.html">Royal Palm Plaza</a></h3>
+								<!-- Tags -->
+								<div class="tags">Branding, Identity</div>
+							</li>
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- END One Fourth -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- One Fourth -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<li class="column-one-fourth portfolio-item identity">
+								<!-- Image -->
+								<a href="portfolio-detail.html" class="image-link"><img alt="" src="images/placeholders/placeholder-large.jpg" class="fullwidth"/></a>
+								<!-- Title -->
+								<h3><a href="portfolio-detail.html">PSB40</a></h3>
+								<!-- Tags -->
+								<div class="tags">Identity</div>
+							</li>
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- END One Fourth -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- One Fourth -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<li class="column-one-fourth portfolio-item coding">
+								<!-- Image -->
+								<a href="portfolio-detail.html" class="image-link"><img alt="" src="images/placeholders/placeholder-large.jpg" class="fullwidth"/></a>
+								<!-- Title -->
+								<h3><a href="portfolio-detail.html">ASPCA</a></h3>
+								<!-- Tags -->
+								<div class="tags">Code Development</div>
+							</li>
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- END One Fourth -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- One Fourth -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<li class="column-one-fourth portfolio-item branding">
+								<!-- Image -->
+								<a href="portfolio-detail.html" class="image-link"><img alt="" src="images/placeholders/placeholder-large.jpg" class="fullwidth"/></a>
+								<!-- Title -->
+								<h3><a href="portfolio-detail.html">Airport Shopping</a></h3>
+								<!-- Tags -->
+								<div class="tags">Branding</div>
+							</li>
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- END One Fourth -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+						</ul>
+					</div>
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<!-- END Carousel -->
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<!-- Latest News / Testimonials (Three Quater/One Fourth) -->
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<div class="column-container">
+
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<!-- Latest News Columns -->
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<div class="column-three-qtr">
+
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- Divider -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<div class="divider"></div>
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- END Divider -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- Title -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<h3 class="section-title">What's going on?</h3>
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- END Title -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- Navigation (Back/Next) -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<div id="blog-nav" class="carousel-nav">
+								<div class="back"></div>
+								<div class="next"></div>
+							</div>
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- END Navigation (Back/Next) -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- The Carousel -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<div class="carousel">
+								<ul id="blog-carousel" class="slider-container">
+
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<!-- One Fourth -->
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<li class="column-one-fourth">
+										<!-- Image -->
+										<a href="blog-post.html" class="image-link"><img alt="" src="images/placeholders/placeholder-large.jpg" class="fullwidth"/></a>
+										<!-- Title -->
+										<h3><a href="blog-post.html">Lorem Ipsum is simply dummy text of the printing</a></h3>
+										<!-- Date -->
+										<div class="date">12th June 2013</div>
+										<!-- Excerpt -->
+										<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of ...</p>
+										<!-- Read More Link -->
+										<a href="blog-post.html">Read More</a>
+									</li>
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<!-- END One Fourth -->
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<!-- One Fourth -->
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<li class="column-one-fourth">
+										<!-- Image -->
+										<a href="blog-post.html" class="image-link"><img alt="" src="images/placeholders/placeholder-large.jpg" class="fullwidth"/></a>
+										<!-- Title -->
+										<h3><a href="blog-post.html">Contrary to popular belief, Lorem Ipsum is not just text</a></h3>
+										<!-- Date -->
+										<div class="date">12th June 2013</div>
+										<!-- Excerpt -->
+										<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of ...</p>
+										<!-- Read More Link -->
+										<a href="blog-post.html">Read More</a>
+									</li>
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<!-- END One Fourth -->
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<!-- One Fourth -->
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<li class="column-one-fourth">
+										<!-- Image -->
+										<a href="blog-post.html" class="image-link"><img alt="" src="images/placeholders/placeholder-large.jpg" class="fullwidth"/></a>
+										<!-- Title -->
+										<h3><a href="blog-post.html">The standard chunk of Lorem Ipsum used since the</a></h3>
+										<!-- Date -->
+										<div class="date">12th June 2013</div>
+										<!-- Excerpt -->
+										<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of ...</p>
+										<!-- Read More Link -->
+										<a href="blog-post.html">Read More</a>
+									</li>
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<!-- END One Fourth -->
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+								</ul>
+							</div>
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- END Carousel -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+						</div>
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<!-- END Latest News Columns -->
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<!-- Testimonials Column -->
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<div class="column-one-fourth">
+
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- Divider -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<div class="divider"></div>
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- END Divider -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- Title -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<h3 class="section-title">Testimonials</h3>
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- END Title -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- Navigation (Back/Next) -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<div id="testimonials-nav" class="carousel-nav">
+								<div class="back"></div>
+								<div class="next"></div>
+							</div>
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- END Navigation (Back/Next) -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- Carousel -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<div class="carousel">
+								<ul id="testimonials-carousel" class="slider-container">
+
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<!-- Testimonial 1 -->
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<li class="column-one-fourth">
+										<!-- Text -->
+										<div class="testimonial-text">
+											<p>"What ever happened to predictability? Everywhere you look, everywhere you go, there's a heart, a hand to hold onto."</p>
+										</div>
+										<!-- Name -->
+										<div class="testimonial-name">
+											Joe Bloggs
+										</div>
+										<!-- Company URL -->
+										<div class="testimonial-link">
+											<a href="#">Company Name</a>
+										</div>
+									</li>
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<!-- END Testimonial 1 -->
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<!-- Testimonial 2 -->
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<li class="column-one-fourth">
+										<!-- Text -->
+										<div class="testimonial-text">
+											<p>"Basix have proven time and time again that they can deliver results on time and on price!"</p>
+										</div>
+										<!-- Name -->
+										<div class="testimonial-name">
+											Joe Bloggs
+										</div>
+										<!-- Company URL -->
+										<div class="testimonial-link">
+											<a href="#">ThemeForest</a>
+										</div>
+									</li>
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+									<!-- END Testimonial 2 -->
+									<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+								</ul>
+							</div>
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+							<!-- END Carousel -->
+							<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+						</div>
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+						<!-- END Testimonials Column -->
+						<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+					</div>
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+					<!-- END Latest News / Testimonials -->
+					<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+
+				</div>
+			</div>
+			<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+			<!-- END Main Content -->
+			<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+			
+@stop
